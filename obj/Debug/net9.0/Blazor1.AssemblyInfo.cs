0aa1c46a9b9150abafa7df7035f1bd8ff2af8921
Johan Magnusson <joma2304@student.miun.se>
2025-01-27 14:37:23 +0100

    gitignore

--- obj/Debug/net9.0/Blazor1.AssemblyInfo.cs
+++ obj/Debug/net9.0/Blazor1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Blazor1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6be9cdc1d9214afe5be5d847cbe0e0231b57ea99")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+11e7f5844677d761738f6e84417837810a6d270b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Blazor1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Blazor1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
